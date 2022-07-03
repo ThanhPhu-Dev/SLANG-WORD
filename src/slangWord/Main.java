424d5a50d2972583c69f030c4a9501b229cd2fb6
Phu Dinh <phu.dinh@inceptionlabs.com.vn>
2022-07-03 23:26:34 +0700

    Chức năng tìm kiếm theo definition, hiển thị ra tất cả các slang words mà trong defintion có chứa keyword gõ vào.

--- src/slangWord/Main.java
+++ src/slangWord/Main.java
@@ -1,9 +1,8 @@
 package slangWord;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -12,18 +11,33 @@ public class Main {
     public static void menu () throws IOException {
         HashMap<String, String> dictionary = Util.readerFile();
         Set<String> set = dictionary.keySet();
-        int choose = 1;
+        int choose = 2;
+        String result = "";
+        String keyword;
         switch (choose){
             case 1:
-                String result = "";
-                System.out.println("Nhap tu muon tim kiem: ");
-                String keyword = scanner.next();
-                String key = set.stream().filter(m -> m.equals(keyword)).findFirst().get();
+                System.out.print("Nhap tu muon tim kiem: ");
+                keyword = scanner.next();
+                String key = set.stream().filter(m -> m.toLowerCase().equals(keyword.toLowerCase())).findFirst().get();
                 if(key != null || !key.isEmpty()){
                     result = dictionary.get(key);
                 }
-                System.out.println(String.format("ket qua tim kiem tu khoa %s la: %s", keyword, result));
-
+                System.out.println(String.format("ket qua tim kiem tu khoa '%s' la: %s", keyword, result));
+                break;
+            case 2:
+                System.out.print("Nhap tu muon tim kiem: ");
+                keyword = scanner.next();
+                Set<String> resultList = dictionary.entrySet().stream().parallel()
+                        .filter(entry -> entry.getValue().contains(keyword))
+                        .map(Map.Entry::getKey)
+                        .collect(Collectors.toSet());
+                if(resultList.size() == 0){
+                    System.out.println("Khong tim thay ket qua voi tu khoa "+ keyword);
+                }else{
+                    System.out.println("Ket qua time kiem voi tu khoa " + keyword);
+                    resultList.forEach(item -> System.out.print(item));
+                }
+                break;
         }
     }
 
