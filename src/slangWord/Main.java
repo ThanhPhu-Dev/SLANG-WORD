06e4199485bfc9afd63c1c2e471a1f6c645f8396
Phu Dinh <phu.dinh@inceptionlabs.com.vn>
2022-07-07 23:19:12 +0700

    Chức năng delete 1 slang word. Confirm trước khi xoá.

--- src/slangWord/Main.java
+++ src/slangWord/Main.java
@@ -7,25 +7,25 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static Scanner scanner = new Scanner(System.in);
-
     public static List<String> history = new ArrayList<>();
 
-    public static void menu () throws IOException {
-        HashMap<String, String> dictionary = Util.readerFile();
-        Set<String> set = dictionary.keySet();
-        int choose = 1;
-        String result = "";
+    public static void menu (HashMap<String, String> dictionary) throws IOException {
+
+        int choose = 6;
+
         String keyword;
         switch (choose){
             case 1:
                 System.out.print("Nhap tu muon tim kiem: ");
                 keyword = scanner.next();
-                String key = set.stream().parallel().filter(m -> (m.toLowerCase()).equals(keyword.toLowerCase())).findFirst().get();
+                String key = Util.getKey(dictionary.keySet(), keyword);
                 if(key != null || !key.isEmpty()){
-                    result = dictionary.get(key);
+                    history.add(keyword);
+                    System.out.println(String.format("ket qua tim kiem tu khoa '%s' la: %s", keyword, dictionary.get(key)));
+                }else{
+                    System.out.println(String.format("Khong tim thay slang word trong he thong", keyword));
                 }
-                history.add(keyword);
-                System.out.println(String.format("ket qua tim kiem tu khoa '%s' la: %s", keyword, result));
+
                 break;
             case 2:
                 System.out.print("Nhap tu muon tim kiem: ");
@@ -52,8 +52,41 @@ public class Main {
                 System.out.format("Nhap dinh nghia cho %s: ", slangWord);
                 String definition = scanner.next();
                 dictionary.put(slangWord, definition);
-                Util.writeToFile(slangWord+"`"+definition, true);
+                Util.writeOneLineToFile(slangWord+"`"+definition);
                 System.out.println("Luu Thanh Cong");
+                break;
+            case 5:
+                System.out.print("Nhap slang word muốn chỉnh sua: ");
+                keyword = Util.getKey(dictionary.keySet(),scanner.next());
+
+                if(keyword == null){
+                    System.out.println("slang word khong tim thay.");
+                    break;
+                }
+                System.out.format("Nhap noi dung chinh sua dinh nghia cho %s: ", keyword);
+                String value = scanner.next();
+                dictionary.put(keyword, value);
+                Util.writeMoreLineToFile(dictionary, "slangCopy.txt");
+                System.out.println("Chinh sua thanh cong");
+                break;
+            case 6:
+                System.out.print("Nhap slang word muon xoa: ");
+                keyword = Util.getKey(dictionary.keySet(),scanner.next());
+
+                if(keyword == null){
+                    System.out.println("slang word khong tim thay.");
+                    break;
+                }
+                System.out.print("Nhap Y de xac nhan xoa. Luu y thao tac nay khong hoan tac: ");
+                if(!scanner.next().toLowerCase().equals("y")){
+                    break;
+                }
+                dictionary.remove(keyword);
+                Util.writeMoreLineToFile(dictionary, false);
+                System.out.println("Xoa thanh cong.");
+                break;
+            case 7:
+                Util.createFileCopy("slangCopy.txt", true);
                 break;
             case 8:
                 Random generator = new Random();
@@ -65,6 +98,8 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        menu();
+        HashMap<String, String> dictionary = Util.init("slang.txt", "slangCopy.txt");
+        menu(dictionary);
+
     }
 }
--- src/slangWord/Util.java
+++ src/slangWord/Util.java
@@ -1,7 +1,11 @@
 package slangWord;
 
 import java.io.*;
-import java.util.HashMap;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
 
 public class Util {
 
@@ -9,8 +13,9 @@ public class Util {
      * @return danh sach key- value doc tu slang.txt
      * @throws IOException
      */
-    public static HashMap<String, String> readerFile() throws IOException {
-        File file = new File(System.getProperty("user.dir") + "\\slang.txt");
+    public static HashMap<String, String> readerFile(String fileName) throws IOException {
+
+        File file = new File(System.getProperty("user.dir") +"\\"+ fileName);
         FileReader fr = new FileReader(file);
         String line = "";
         HashMap<String, String> dictionary = new HashMap<>();
@@ -39,22 +44,122 @@ public class Util {
         }
     }
 
-    public static void writeToFile(String content, boolean isAppend){
-        try{
-            File file = new File(System.getProperty("user.dir") + "\\slang.txt");
-            if(!file.exists()){
+    public static void writeOneLineToFile(String content) {
+        File file = null;
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+            file = new File(System.getProperty("user.dir") + "\\slangCopy.txt");
+            if (!file.exists()) {
                 file.createNewFile();
             }
 
-            FileWriter fw = new FileWriter(file, isAppend);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(content);
-            bw.write("\n");
-            bw.close();
-            fw.close();
-        }catch (Exception e){
+            fw = new FileWriter(file, true);
+            bw = new BufferedWriter(fw);
+            writeContentToFile(bw,content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String getKey(Collection<String> set, String keyword){
+        return set.stream().parallel().filter(m -> (m.toLowerCase()).equals(keyword.toLowerCase())).findFirst().get();
+    }
+
+    public static void writeMoreLineToFile(Map<String,String> map, String fileName){
+        File file = null;
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+
+            file = new File(System.getProperty("user.dir") + "\\"+ fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            fw = new FileWriter(file, false);
+            bw = new BufferedWriter(fw);
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                writeContentToFile(bw,entry.getKey() + "`" + entry.getValue());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    private static void writeContentToFile(BufferedWriter bw, String content) throws IOException {
+        bw.write(content);
+        bw.write("\n");
+    }
+
+    public static void createFileCopy(String fileNameSource, String fileNameTarget, boolean isReplace) {
+        File file = new File(System.getProperty("user.dir") + "\\" + fileNameTarget);
+        try {
+            if(!isReplace){
+                if(!file.exists()){
+                    file.createNewFile();
+                    Path source = Paths.get(System.getProperty("user.dir") + "\\" + fileNameSource);
+                    Path target = Paths.get(System.getProperty("user.dir") + "\\" + fileNameTarget);
+
+                }
+            }
+            if (!file.exists()) {
+                file.createNewFile();
+                Path source = Paths.get(file.getPath());
+                Path target = Paths.get(System.getProperty("user.dir") + "\\" + "slangCopy.txt");
+                Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+            }else {
+                if (isReplace) {
+                    Path source = Paths.get(file.getPath());
+                    Path target = Paths.get(System.getProperty("user.dir") + "\\" + "slangCopy.txt");
+                    Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static HashMap<String, String> init(String fileSourceName, String fileTargetName){
+        File file = new File(System.getProperty("user.dir") + "\\" + fileTargetName);
+        HashMap<String, String> dictionary = null;
+        try{
+
+            if(!file.exists()){
+                file.createNewFile();
+                dictionary = readerFile(fileSourceName);
+                writeMoreLineToFile(dictionary, file.getName() + ".txt");
+            }else{
+                dictionary = readerFile(file.getName() + ".txt");
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return dictionary;
     }
 
 }
